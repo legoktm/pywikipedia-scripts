5d51769cdb3d0b52aefd9591c2187932311c3bb1
Kunal Mehta <legoktm@gmail.com>
2012-06-28 23:37:19 -0700

    pylint friendly

--- pui.py
+++ pui.py
@@ -2,17 +2,18 @@
 # -*- coding: utf-8  -*
 #$ -m ae
  
-#
-# (C) Legoktm 2008-2012, MIT License
-# Originally written for Pywikipedia, converted over to rewrite branch.
-# 
+"""
+(C) Legoktm 2008-2012, MIT License
+Originally written for Pywikipedia, converted over to rewrite branch.
+"""
  
 import re
 import pywikibot
 
-site=pywikibot.getSite()
+site = pywikibot.getSite()
 page = pywikibot.Page(site,'Wikipedia:Possibly unfree files')
 wikitext = page.get()
-wikitext = re.compile(r'\n==New listings==', re.IGNORECASE).sub(r'\n*[[/{{subst:#time:Y F j|-14 days}}]]\n==New listings==', wikitext)
+search = re.compile(r'\n==New listings==', re.IGNORECASE)
+wikitext = search.sub(r'\n*[[/{{subst:#time:Y F j|-14 days}}]]\n==New listings==', wikitext)
 EditMsg = 'Adding new day to holding cell'
-page.put(wikitext,EditMsg)
+page.put(wikitext, EditMsg)
