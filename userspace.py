b840f2677ec93d42905bc093db3a1d64fe1b549b
Kunal Mehta <legoktm@gmail.com>
2012-06-28 23:41:56 -0700

    pylint cleanup

--- userspace.py
+++ userspace.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
-#
-# (C) 2012, Legoktm, under the MIT License
-# Checks the new pages list to see whether it should have been made in the userspace, and if so, moves it.
-#
+
+"""
+(C) 2012, Legoktm, under the MIT License
+Checks the new pages list to see whether
+it should have been made in the userspace, and if so, moves it.
+"""
+
 import sys
 
 import pywikibot
@@ -39,21 +42,17 @@ def main():
       history = page.getVersionHistory()
       for item in history:
         if item[2] == u'Legobot':
-          print 'We have already touched %s. Skipping!' %(page.title())
+          print 'We have already touched %s. Skipping!' % (page.title())
           continue
       trial_count()
       page.move(new, reason='BOT: Moving accidentally created subpage into userspace')
-      #delete newly created redirect
-      #Apparently bots can move pages without creating redirects so this part isnt needed...
-#      oldpage = pywikibot.Page(site, old)
-#      oldpage.put('{{db-r2}}', 'BOT: Nominating for deletion per [[WP:CSD#R2|CSD]]')
       #Leave a talk-page notice
       talk = pywikibot.Page(site, 'User talk:' + creator)
-      notice = '{{subst:User:Legobot/userfy move|1=%s|2=%s}} ~~~~' %(old, new)
+      notice = '{{subst:User:Legobot/userfy move|1=%s|2=%s}} ~~~~' % (old, new)
       existing = talk.get()
-      talk.put(existing+notice, 'Bot moved [[%s]] to [[%s]]' %(old, new), minorEdit=False)
+      talk.put(existing+notice, 'Bot moved [[%s]] to [[%s]]' % (old, new), minorEdit=False)
     else:
-      print 'Skipping %s' %page.title(asLink=True)
+      print 'Skipping %s' % page.title(asLink=True)
       continue
 
 if __name__ == "__main__":
