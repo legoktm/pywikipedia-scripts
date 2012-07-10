0fecda111786c6af691ba19594412568b757fd99
Kunal Mehta <legoktm@gmail.com>
2012-07-10 15:34:21 -0700

    fix unicode errors

--- afcmove.py
+++ afcmove.py
@@ -45,25 +45,23 @@ def main():
     new = item['new']
     user = item['user']
     if new.startswith('Articles for creation/'):
-      print 'Will log %s --> %s' % (old, new)
       log = True
     if old.startswith('Wikipedia talk:Articles for creation/') and new.startswith('Wikipedia talk:'):
-      print 'Will log %s --> %s' % (old, new)
       log = True
     if old.startswith('Wikipedia:Articles for creation/') and new.startswith('Wikipedia:'):
-      print 'Will log %s --> %s' % (old, new)
       log = True
     if not log:
-      print 'Skipping %s --> %s' % (old, new)
+      print u'Skipping %s --> %s' % (old, new)
       continue
-    logtext += '* [[%s]] --> [[%s]] by [[User:%s|]]\n' % (old, new, user)
-  if logtext == '':
-    print 'Nothing was detected, won\'t update the log.'
+    print u'Will log %s --> %s' % (old, new)
+    logtext += u'* [[%s]] --> [[%s]] by [[User:%s|]]\n' % (old, new, user)
+  if logtext == u'':
+    print u'Nothing was detected, won\'t update the log.'
     return
-  p = pywikibot.Page(site, 'Wikipedia:Articles for creation/Wrongly moved submissions')
+  p = pywikibot.Page(site, u'Wikipedia:Articles for creation/Wrongly moved submissions')
   current_text = p.get()
   newtext = current_text + logtext
-  p.put(newtext, 'BOT: Updating log')
+  p.put(newtext, u'BOT: Updating log')
     
     
 if __name__ == "__main__":
