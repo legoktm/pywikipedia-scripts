d6c06bd1510fa5f0b9dff70291d5ed1c71c5c17d
Kunal Mehta <legoktm@gmail.com>
2012-11-04 12:19:51 -0600

    convert into a class structure, some parsing still broken

--- req_article_cleanup.py
+++ req_article_cleanup.py
@@ -20,78 +20,110 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 IN THE SOFTWARE.
 """
+
 import pywikibot
+import robot
 import mwparserfromhell
 import tagger
-site = pywikibot.Site()
+
 TagBot = tagger.TaggerBot('WikiProject Requested articles')
 TagBot.init()
 
-def gen():
-    page = pywikibot.Page(site, 'Wikipedia:Requested articles')
-    links = page.linkedPages(namespaces=[4])
-    for pg in links:
-        if pg.title().startswith('Wikipedia:Requested articles/'):
-            yield pg
+class RAUpdaterBot(robot.Robot):
+    def __init__(self):
+        robot.Robot.__init__(self, task=26)
+        self.TagBot = tagger.TaggerBot('WikiProject Requested articles')
+        self.TagBot.init()
+        self.edit_count = 11
 
 
-def parse_page(page):
-    print page
-    content = page.get()
-    lines = content.splitlines()
-    delete_these = list()
-    for item in lines:
-        try:
-            if parse_line(item):
-                delete_these.append(item)
-        except pywikibot.exceptions.InvalidTitle:
-            pass
-    for l in delete_these:
-        del lines[lines.index(l)]
-    newtext = '\n'.join(lines)
-    pywikibot.showDiff(content, newtext)
-    page.put(newtext, 'Bot: removing completed requests. Want to help? Join the [[Wikipedia:WikiProject Requested articles/Backlog Drive|WikiProject Requested articles Backlog Drive]] today!')
-    quit()
+    def gen(self):
+        page = pywikibot.Page(self.site, 'Wikipedia:Requested articles')
+        links = page.linkedPages(namespaces=[4])
+        for pg in links:
+            if pg.title().startswith('Wikipedia:Requested articles/'):
+                yield pg
 
 
-def parse_line(line):
-    if line.isspace():
-        return
-    elif line.startswith('=='):
-        return
-    name = None
-    code = mwparserfromhell.parse(line)
-    for template in code.filter_templates():
-        if template.name.lower().strip() == 'req':
-            name = template.get(1).value
-            break
-    if not name:
-        #find the first wikilink and check it
-        key1 = line.find('[[')
-        if key1 == -1:
-            #no links???
-            return
-        p1 = line[key1+2]
-        key2 = p1.find(']]')
-        if key2 == -1:
-            #no ending????
-            return
-        p2 = p1[:key2]
-        name = p2.split('|')[0] #fix piped links
-    pg = pywikibot.Page(site, name)
-    print pg
-    if pg.exists():
-        if (not pg.isRedirectPage()) and (not pg.isDisambig()):
-            #IT EXISTS
-            print pg.title()+' exists!'
-            talk = pg.toggleTalkPage()
-            TagBot.tag_page(talk)
+    def parse_page(self, page):
+        print page
+        content = page.get()
+        lines = content.splitlines()
+        delete_these = list()
+        for item in lines:
+            try:
+                if self.parse_line(item):
+                    delete_these.append(item)
+            except pywikibot.exceptions.InvalidTitle:
+                pass
+            except IndexError:
+                pass
+        for l in delete_these:
+            del lines[lines.index(l)]
+        newtext = '\n'.join(lines)
+        pywikibot.showDiff(content, newtext)
+        if content != newtext:
+            page.put(newtext, 'Bot: removing completed requests. Want to help? Join the [[Wikipedia:WikiProject Requested articles/Backlog Drive|WikiProject Requested articles Backlog Drive]] today!')
             return True
-    #print pg.title()+' doesn\'t exist :('
+        return False
+
+
+    def parse_line(self, line):
+        if line.isspace():
+            return
+        elif line.startswith('=='):
+            return
+        name = None
+        code = mwparserfromhell.parse(line)
+        for template in code.filter_templates():
+            if template.name.lower().strip() == 'req':
+                name = template.get(1).value
+                break
+        if not name:
+            #find the first wikilink and check it
+            key1 = line.find('[[')
+            if key1 == -1:
+                #no links???
+                return
+            p1 = line[key1+2]
+            key2 = p1.find(']]')
+            if key2 == -1:
+                #no ending????
+                return
+            p2 = p1[:key2]
+            name = p2.split('|')[0] #fix piped links
+        pg = pywikibot.Page(self.site, name)
+        print pg
+        if pg.exists():
+            if (not pg.isRedirectPage()) and (not pg.isDisambig()):
+                #IT EXISTS
+                print pg.title()+' exists!'
+                talk = pg.toggleTalkPage()
+
+                if self.TagBot.tag_page(talk):
+                    self.edit_count += 1
+                return True
+        #print pg.title()+' doesn\'t exist :('
+    def run(self):
+        self.updated = list()
+        for page in self.gen():
+            res = self.parse_page(page)
+            if res:
+                self.edit_count += 1
+                self.updated.append(page)
+            if self.edit_count >= 50:
+                break
+
 
 
 
 if __name__ == "__main__":
-    for page in gen():
-        parse_page(page)
+    bot = RAUpdaterBot()
+    try:
+        bot.run()
+    finally:
+        log = pywikibot.Page(bot.site, 'User:Legobot/Logs/26')
+        text = log.text +'\n' + '\n'.join(['* Updated [[%s]]' % p.title() for p in bot.updated])
+        log.put(text, 'Bot: Updating log in userspace')
+
     #parse_page(pywikibot.Page(site, 'Wikipedia:Requested articles/Applied arts and sciences/Medicine'))
