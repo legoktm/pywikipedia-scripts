a7cac4a13225c30b2c58b6ab65da03ee646a0e50
Kunal Mehta <legoktm@member.fsf.org>
2016-11-17 11:25:51 -0800

    Fix/rewrite oldidGA

--- oldidGA.py
+++ oldidGA.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -
 """
 Copyright (C) 2012 Legoktm
@@ -25,18 +25,15 @@ from __future__ import unicode_literals
 # See [[Wikipedia:Bot_requests/Archive_49#Bot_request_for_adding_oldid_parameters_to_good_articles]]
 import re
 import pywikibot
-import robot
 
 
-class OldidGABot(robot.Robot):
-
+class OldidGABot(object):
     def __init__(self):
-        robot.Robot.__init__(self, task=18)
+        self.site = pywikibot.Site('en', 'wikipedia')
 
     def run(self):
-        self.startLogging(pywikibot.Page(self.site, 'User:Legobot/GA'))
-        page = pywikibot.Page(self.site, 'Category:Good articles without an oldid')
-        category = pywikibot.Category(page)
+        cat_page = pywikibot.Page(self.site, 'Category:Good articles without an oldid')
+        category = pywikibot.Category(cat_page)
         gen = category.articles()
         for page in gen:
             self.process_page(page)
@@ -44,27 +41,35 @@ class OldidGABot(robot.Robot):
     def process_page(self, page):
         talk_page = page
         page = talk_page.toggleTalkPage()
-        #find the edit where {{good article}] was added
+        # find the edit where {{good article}] was added
         found_oldid = False
         oldid = None
         while not found_oldid:
             self.site.loadrevisions(page, getText=True, rvdir=False,
                                     step=10, total=10, startid=oldid)
-            hist = page.fullVersionHistory(total=10)  # This should fetch nothing...
-            for revision in hist:
-                if re.search('\{\{(good|ga) article\}\}', revision[3], re.IGNORECASE):
+            for revid, rev in sorted(page._revisions.items(), reverse=True):
+                revision = rev.full_hist_entry()
+                print(revision[0])
+                if revision[3] is None:
+                    oldid = revision[0]
+                    continue
+#                print(revision[3].splitlines()[0])
+                if re.search('\{\{(good|ga) (icon|article)\}\}', revision[3], re.IGNORECASE):
                     oldid = revision[0]
                 else:
-                    #current oldid is the right one
+                    # current oldid is the right one
                     found_oldid = True
                     break
-        #add the oldid in the template
+        # add the oldid in the template
         if not oldid:
-            self.output('* ERROR: Could not find oldid for [[%s]]' % talk_page.title())
+            print('* ERROR: Could not find oldid for [[%s]]' % talk_page.title())
             return
-        self.output('* Adding |oldid=%s to [[%s]]' % (oldid, talk_page.title()))
+        print('* Adding |oldid=%s to [[%s]]' % (oldid, talk_page.title()))
         oldtext = talk_page.get()
         search = re.search('\{\{GA\s?\|(.*?)\}\}', oldtext)
+        if not search:
+            print('* ERROR: Could not find template to replace for [[%s]]' % talk_page.title())
+            return
         newtext = oldtext.replace(search.group(0), '{{GA|%s|oldid=%s}}' % (search.group(1), oldid))
         pywikibot.showDiff(oldtext, newtext)
         talk_page.put(newtext, 'BOT: Adding |oldid=%s to {{[[Template:GA|GA]]}}' % oldid)
@@ -72,7 +77,4 @@ class OldidGABot(robot.Robot):
 
 if __name__ == "__main__":
     bot = OldidGABot()
-    try:
-        bot.run()
-    finally:
-        bot.pushLog()
+    bot.run()
